e945d4ddc8227e963ed0a0ab1fc2c2f2f0ddf77d
julia-kaminska <kaminska.julia@interia.pl>
2023-02-26 10:01:15 +0100

    Airport entity

---
+++ src/main/java/practicalProject/model/Airport.java
@@ -0,0 +1,62 @@
+package practicalProject.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Airport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private int id;
+
+    @Column (name = "airport_name")
+    private String airportName;
+
+    @Column (name = "country")
+    private String country;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAirportName() {
+        return airportName;
+    }
+
+    public void setAirportName(String airportName) {
+        this.airportName = airportName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Airport(int id, String airportName, String country) {
+        this.id = id;
+        this.airportName = airportName;
+        this.country = country;
+    }
+
+    public Airport(String airportName, String country) {
+        this.airportName = airportName;
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Airport{" +
+                "id=" + id +
+                ", airportName='" + airportName + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
